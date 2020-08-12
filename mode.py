new push after restructure on gpu server, also fix of all zero gradients

mode.py
@@ -7,48 +7,74 @@ import logging
 from utils import visualize, set_config
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
+import time
 
 config = Config.get_instance()
 criterion = nn.CrossEntropyLoss()
 
 if torch.cuda.is_available():
-    device = torch.device("cuda:1")
+    device = torch.device(config["Setup"]["Device"])
+    if torch.cuda.device_count() > 1:
+        config["Setup"]["Parallel"] = 1
+    else:
+        config["Setup"]["Parallel"] = 0
 else:
     device = torch.device("cpu")
 
 
-def train(net, dataset):
+def train(net, dataset, return_data=False):
 
-    writer_name = (
-        net.name
-        + "_"
-        + dataset.name
-        + "_"
-        + datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
-    )
-    writer = SummaryWriter("./data/models/log/runs/" + writer_name + "/train")
+    if config["Setup"]["Parallel"] == 1:
+        writer_name = (
+            net.module.name
+            + "_"
+            + dataset.name
+            + "_"
+            + datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
+        )
+        writer = SummaryWriter("./data/models/log/runs/" + writer_name + "/train")
 
-    net.writer = writer_name
+        net.module.writer = writer_name
+    else:
+        writer_name = (
+            net.name
+            + "_"
+            + dataset.name
+            + "_"
+            + datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
+        )
+        writer = SummaryWriter("./data/models/log/runs/" + writer_name + "/train")
+
+        net.writer = writer_name
+
     optimizer = set_config.choose_optimizer(net)
 
-    for epoch in range(
-        config["Setup"]["Epochs"]
-    ):  # loop over the dataset multiple times
+    loss_list = []
+    accuracy_list = []
+    f_score_list = []
+    iter_list = []
+
+    _global_start = time.time()
+
+    logging.info("Started Training")
+
+    _epoch_process_time = 0
+
+    for epoch in range(config["Setup"]["Epochs"]):
 
         running_loss = 0.0
         correct = 0
         total = 0
         cumulated_labels = []
         cumulated_predictions = []
-        for i, data in enumerate(dataset.trainloader, 0):
 
-            if i > 50:
-                break
+        _epoch_start = time.time()
+
+        for i, data in enumerate(dataset.trainloader):
+            optimizer.zero_grad()
 
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
-
-            optimizer.zero_grad()
 
             outputs = net(inputs)
 
@@ -60,70 +86,80 @@ def train(net, dataset):
 
             _, predicted = torch.max(outputs.data, 1)
 
-            cumulated_labels.extend(labels.tolist())
-            cumulated_predictions.extend(predicted.tolist())
-
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-            if i % 10 == 9:
+            if i % 100 == 99:
+                loss_avg = running_loss / 100
+                acc = 100 * correct / total
+                iteration = epoch * len(list(dataset.trainloader)) + i
                 writer.add_scalar(
-                    "Loss/Train",
-                    running_loss / 10,
-                    epoch * len(dataset.trainloader) + i,
+                    "Loss/Train", loss_avg, iteration,
                 )
-                writer.add_scalar(
-                    "Accuracy/Train",
-                    100 * correct / total,
-                    epoch * len(dataset.trainloader) + i,
-                )
-                writer.add_scalar(
-                    "F-Score/Train",
-                    100
-                    * f1_score(
-                        y_true=cumulated_labels,
-                        y_pred=cumulated_predictions,
-                        average="macro",
-                    ),
-                    epoch * len(dataset.trainloader) + i,
-                )
-                # writer.add_figure(
-                #    "Predictions vs. Truth",
-                #    visualize.plot_classes_preds(net, inputs, labels, dataset.classes),
-                #    global_step=epoch * len(dataset.trainloader) + i,
-                # )
+                writer.add_scalar("Accuracy/Train", acc, iteration)
+                logging.info("Loss:\t%.4f\tAcc:\t%.4f" % (loss_avg, acc))
+                iter_list.append(iteration)
+                loss_list.append(loss_avg)
+                accuracy_list.append(acc)
 
                 running_loss = 0.0
                 correct = 0
                 total = 0
-                cumulated_labels = []
-                cumulated_predictions = []
-    print("Finished Training")
-    PATH = (
-        "./data/models/save/"
-        + net.name
-        + "_"
-        + dataset.name
-        + "_"
-        + datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
-        + ".pth"
+
+        logging.info("Finished Epoch %i / %i" % (epoch + 1, config["Setup"]["Epochs"]))
+
+        _epoch_end = time.time()
+
+        _epoch_time = _epoch_end - _epoch_start
+
+        _epoch_process_time += _epoch_time
+        logging.info("Time for epoch:\t%8.8f" % (_epoch_time))
+
+    logging.info("Finished Training")
+    _global_end = time.time()
+
+    _global_process_time = _global_end - _global_start
+
+    logging.info(
+        "Average time for each epoch:\t%8.8f"
+        % (_epoch_process_time / config["Setup"]["Epochs"])
     )
+    logging.info("Time needed for training:\t%8.8f" % (_global_process_time))
+    if config["Setup"]["Parallel"] == 1:
+        PATH = (
+            "./data/models/save/"
+            + net.module.name
+            + "_"
+            + dataset.name
+            + "_"
+            + datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
+            + ".pth"
+        )
+    else:
+        PATH = (
+            "./data/models/save/"
+            + net.name
+            + "_"
+            + dataset.name
+            + "_"
+            + datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
+            + ".pth"
+        )
+
     torch.save(net.state_dict(), PATH)
-    return net
+
+    if return_data:
+        return net, iter_list, loss_list, accuracy_list
+    else:
+        return net
 
 
-def test(net, dataset):
+def test(net, dataset, return_data=False):
 
-    writer = SummaryWriter("./data/models/log/runs/" + net.writer + "/test")
-
-    correct = 0
-    total = 0
-    num_classes = len(dataset.classes)
-    class_correct = list(0.0 for i in range(num_classes))
-    class_total = list(0.0 for i in range(num_classes))
-    running_loss = 0.0
-    cumulated_labels = []
-    cumulated_predictions = []
+    if config["Setup"]["Parallel"] == 1:
+        writer = SummaryWriter("./data/models/log/runs/" + net.module.writer + "/test")
+    else:
+        writer = SummaryWriter("./data/models/log/runs/" + net.writer + "/test")
 
     final_correct = 0
     final_total = 0
@@ -134,68 +170,27 @@ def test(net, dataset):
     with torch.no_grad():
         for idx, data in enumerate(dataset.testloader):
 
-            if idx > 10:
-                break
-
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
             outputs = net(inputs)
 
             loss = criterion(outputs, labels)
-            running_loss += loss.item()
             final_running_loss += loss.item()
 
             _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
 
             final_total += labels.size(0)
             final_correct += (predicted == labels).sum().item()
 
-            c = (predicted == labels).squeeze()
-
-            cumulated_labels.extend(labels.tolist())
-            cumulated_predictions.extend(predicted.tolist())
-
             final_cumulated_labels.extend(labels.tolist())
             final_cumulated_predictions.extend(predicted.tolist())
 
-            if idx % 10 == 9:
-                writer.add_scalar(
-                    "Loss/Test", running_loss / 10, len(dataset.trainloader) + idx
-                )
-                writer.add_scalar(
-                    "Accuracy/Test",
-                    100 * correct / total,
-                    len(dataset.trainloader) + idx,
-                )
-                writer.add_scalar(
-                    "F-Score/Test",
-                    100
-                    * f1_score(
-                        y_true=cumulated_labels,
-                        y_pred=cumulated_predictions,
-                        average="macro",
-                    ),
-                    len(dataset.trainloader) + idx,
-                )
+    logging.info("Final Metrics")
 
-                running_loss = 0.0
-                correct = 0
-                total = 0
-                cumulated_labels = []
-                cumulated_predictions = []
-
-            for i in range(len(labels)):
-                label = labels[i]
-                class_correct[label] += c[i].item()
-                class_total[label] += 1
-
-    print("Final Metrics")
-    print(
+    logging.info(
         "Loss: %.3f\tAccuracy: %.3f%%\tF_Score: %.3f%%"
         % (
-            final_running_loss / 10,
+            final_running_loss / (idx + 1),
             100 * final_correct / final_total,
             100
             * f1_score(
@@ -205,11 +200,7 @@ def test(net, dataset):
             ),
         )
     )
-    for i in range(num_classes):
-        print(
-            "Accuracy of %5s : %2d %%"
-            % (dataset.classes[i], 100 * class_correct[i] / class_total[i])
-        )
+    return
 
 
 def production(net, data):

run.py
@@ -1,24 +1,35 @@
 from utils import load_data, set_config, visualize
+from utils.config import Config
 import mode
 import torch
+import torch.nn as nn
 import logging
+
+config = Config.get_instance()
 
 logging.basicConfig(level=logging.INFO)
 
-data = load_data.get_data()
+data = load_data.get_data(augment=config["DataLoader"]["Augment"])
 
 logging.info("Loaded Dataset")
 
 if torch.cuda.is_available():
-    device = torch.device("cuda:1")
-    logging.info("Running on the GPU")
+    device = torch.device(config["Setup"]["Device"])
+    logging.info("Running on %i GPUs" % (torch.cuda.device_count()))
 else:
     device = torch.device("cpu")
-    logging.info("Running on the CPU")
+    logging.info("Running on CPU")
 
 net = set_config.choose_architecture()
 
+if torch.cuda.device_count() > 1:
+    net = nn.DataParallel(net)
+    config["Setup"]["Parallel"] = 1
+else:
+    config["Setup"]["Parallel"] = 0
+
 net = net.to(device)
+
 
 logging.info("Initialized Network")
 

utils/load_data.py
@@ -2,10 +2,15 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 from utils.config import Config
+import multiprocessing
 
 config = Config.get_instance()
 
 data_name = config["Setup"]["Data"]
+
+global worker_count
+# worker_count = multiprocessing.cpu_count()
+worker_count = 3
 
 
 class dataset:
@@ -17,6 +22,61 @@ class dataset:
 
 
 def get_data(augment=False):
+    if data_name == "mnist":
+        mean = (0.49139968,)
+        std = (0.24703233,)
+        if augment:
+            tr_transform = transforms.Compose(
+                [
+                    transforms.RandomCrop(32, padding=4),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean, std),
+                ]
+            )
+        else:
+            tr_transform = transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize(mean, std),]
+            )
+        val_transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(mean, std),]
+        )
+        trainset = torchvision.datasets.MNIST(
+            root="./data/datasets", train=True, download=True, transform=tr_transform,
+        )
+
+        testset = torchvision.datasets.MNIST(
+            root="./data/datasets", train=False, download=True, transform=val_transform,
+        )
+        classes = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+    if data_name == "svhn":
+        mean = [0.5, 0.5, 0.5]
+        std = [0.5, 0.5, 0.5]
+        if augment:
+            tr_transform = transforms.Compose(
+                [
+                    transforms.RandomCrop(32, padding=4),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean, std),
+                ]
+            )
+        else:
+            tr_transform = transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize(mean, std),]
+            )
+        val_transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(mean, std),]
+        )
+        trainset = torchvision.datasets.SVHN(
+            root="./data/datasets", train=True, download=True, transform=tr_transform,
+        )
+
+        testset = torchvision.datasets.SVHN(
+            root="./data/datasets", train=False, download=True, transform=val_transform,
+        )
+        classes = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
     if data_name == "cifar10":
         mean = [0.49139968, 0.48215827, 0.44653124]
         std = [0.24703233, 0.24348505, 0.26158768]
@@ -41,16 +101,8 @@ def get_data(augment=False):
             root="./data/datasets", train=True, download=True, transform=tr_transform
         )
 
-        trainloader = torch.utils.data.DataLoader(
-            trainset, batch_size=64, shuffle=True, num_workers=2
-        )
-
         testset = torchvision.datasets.CIFAR10(
             root="./data/datasets", train=False, download=True, transform=val_transform
-        )
-
-        testloader = torch.utils.data.DataLoader(
-            testset, batch_size=64, shuffle=False, num_workers=2
         )
 
         classes = (
@@ -90,16 +142,8 @@ def get_data(augment=False):
             root="./data/datasets", train=True, download=True, transform=tr_transform
         )
 
-        trainloader = torch.utils.data.DataLoader(
-            trainset, batch_size=64, shuffle=True, num_workers=2
-        )
-
         testset = torchvision.datasets.CIFAR100(
             root="./data/datasets", train=False, download=True, transform=val_transform
-        )
-
-        testloader = torch.utils.data.DataLoader(
-            testset, batch_size=64, shuffle=False, num_workers=2
         )
 
         classes = (
@@ -205,4 +249,20 @@ def get_data(augment=False):
             "tractor",
         )
 
+    trainloader = torch.utils.data.DataLoader(
+        trainset,
+        batch_size=config["DataLoader"]["TrainSize"],
+        shuffle=True,
+        num_workers=config["DataLoader"]["Num_Workers"],
+        pin_memory=config["DataLoader"]["Pin_Memory"],
+        drop_last=config["DataLoader"]["Drop_Last"],
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset,
+        batch_size=config["DataLoader"]["TestSize"],
+        shuffle=False,
+        pin_memory=config["DataLoader"]["Pin_Memory"],
+        drop_last=config["DataLoader"]["Drop_Last"],
+        num_workers=config["DataLoader"]["Num_Workers"],
+    )
     return dataset(data_name, trainloader, testloader, classes)

utils/visualize.py
@@ -13,24 +13,31 @@ config = Config.get_instance()
 # matplotlib.use("Qt5Agg")
 
 if torch.cuda.is_available():
-    device = torch.device("cuda")
+    device = torch.device(config["Setup"]["Device"])
 else:
     device = torch.device("cpu")
 
 
 def rev_normalize(img):
     if config["Setup"]["Data"] == "cifar10":
-        return img / 2 + 0.5
+        std = [0.24703233, 0.24348505, 0.26158768]
+        mean = [0.49139968, 0.48215827, 0.44653124]
+        img[0, :, :] = img[0, :, :] * std[0] + mean[0]
+        img[1, :, :] = img[1, :, :] * std[1] + mean[1]
+        img[2, :, :] = img[2, :, :] * std[2] + mean[2]
+        return img
 
 
-def imshow(img):
+def imshow(img, show=True):
     img = rev_normalize(img)
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+    if show:
+        plt.show()
+    return
 
 
-def show(dataset, nimages=4, net=None):
+def show(dataset, nimages=4, net=None, pltshow=True):
 
     dataiter = iter(dataset.testloader)
     images, labels = dataiter.next()
@@ -38,7 +45,7 @@ def show(dataset, nimages=4, net=None):
 
     classes = dataset.classes
 
-    imshow(torchvision.utils.make_grid(images))
+    imshow(torchvision.utils.make_grid(images), show=pltshow)
     print("GroundTruth: ", " ".join("%5s" % classes[labels[j]] for j in range(4)))
 
     if net is not None: