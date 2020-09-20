c5d4749f2f0062917a1a8f563e994437d0c2d86b
skallenberg <s.kallenberg@outlook.com>
2020-09-20 17:28:55 +0200

    sorted versions

---
+++ model/v2/__init__.py
@@ -0,0 +1,6 @@
+from .ResNetv2 import *
+from .BaseNetv2 import *
+from .DenseNetv2 import *
+from .FractalNetv2 import *
+from .MultilevelResidualsv2 import *
+from .MultigridArchitecturesv2 import *
