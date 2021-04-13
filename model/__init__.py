cc362fb29e23b6279ae9f9103ffd16c954c8e5b4
SeanKallenberg-Previsionz <72306839+SeanKallenberg-Previsionz@users.noreply.github.com>
2021-04-13 11:11:17 +0200

    update

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,5 @@ from .ResNet import *
 from .BaseNet import *
 from .DenseNet import *
 from .FractalNet import *
-from .NetworkInNetwork import *
 from .MultilevelResiduals import *
-from .MultigridArchitectures import *
 from .MGNet import *
